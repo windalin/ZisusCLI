57ebbd7c1aa4d6f6ce7839b30591747ddcf29cd6
windalin <45084896+windalin@users.noreply.github.com>
2026-08-02 01:56:23 +1200

    return 1

--- ZisusCLI/Program.cs
+++ ZisusCLI/Program.cs
@@ -4,16 +4,9 @@ using System.Threading.Tasks;
 namespace ZisusCLI {
 	internal class Program {
 		static async Task<int> Main(string[] args) {
-
-			if (args.Length == 0) {
-				await Console.Out.WriteLineAsync("Usage: ZisusCLI <command>");
-				await Console.Out.WriteAsync("Waiting for input: ");
-				var i = await Console.In.ReadLineAsync();
-				return 1;
-			}
-
+			if (args.Length == 0) return 1;
+			
 			string command = args[0];
-
 			switch (command) {
 				case "amigos":
 					return new Commands.Amigos().Run();
